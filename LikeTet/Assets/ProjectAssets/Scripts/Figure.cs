8fa1916fd2ee1e2f4c0a147a633548dd676732b3
Egrolius <egolius3d@gmail.com>
2017-06-05 22:22:28 +0300

    Add moving script for basic shapes

---
+++ LikeTet/Assets/ProjectAssets/Scripts/Figure.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Figure : MonoBehaviour {
+
+	public float fallSpeed=1;
+	float fall=0;
+
+	
+	// Update is called once per frame
+	void Update () {
+		CheckUserInput ();
+	}
+
+	void CheckUserInput()
+	{
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			this.transform.position += new Vector3 (-1,0,0);
+
+		} 
+		else if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			this.transform.position += new Vector3 (1,0,0);
+		}
+		else if (Input.GetKeyDown (KeyCode.DownArrow)|| Time.time-fall>=fallSpeed) 
+		{
+			this.transform.position += new Vector3 (0,-1,0);
+			fall = Time.time;
+		}
+		else if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			this.transform.Rotate (0,0,90);
+		}
+	}
+	public static bool isInsideGrid ()
+	{
+		return true;
+	}
+}
--- LikeTet/Assets/ProjectAssets/Scripts/GameState.cs
+++ LikeTet/Assets/ProjectAssets/Scripts/GameState.cs
@@ -4,30 +4,17 @@ using UnityEngine;
 namespace LikeTet
 {
 	public class GameState : AppState {
-		
-		
+
+		public static int gridWeight = 10;
+		public static int gridHeight = 20;
+		public static Transform[,] grid = new Transform [gridWeight,gridHeight];
+
 		public void Update()
 		{
-			CheckUserInput ();
+			
 		}
-		void CheckUserInput()
-		{
-			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-				this.transform.position += new Vector3 (-1,0,0);
-			} 
-			else if (Input.GetKeyDown (KeyCode.RightArrow)) 
-			{
-				
-			}
-			else if (Input.GetKeyDown (KeyCode.DownArrow)) 
-			{
+			
 
-			}
-			else if (Input.GetKeyDown (KeyCode.Space)) 
-			{
-
-			}
-		}
 }
 
 }
